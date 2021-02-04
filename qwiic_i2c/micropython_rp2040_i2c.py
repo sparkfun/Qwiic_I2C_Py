f430fe287ba6ce00a2d7122fada00792c17b8712
oclyke <owen.lyke@sparkfun.com>
2021-02-03 17:50:50 -0700

    Create micropython_rp2040_i2c.py

---
+++ qwiic_i2c/micropython_rp2040_i2c.py
@@ -0,0 +1,149 @@
+#-----------------------------------------------------------------------------
+# micropython_rp2040_i2c.py
+#
+# Encapsulate RP2040 MicroPython port I2C interface
+#------------------------------------------------------------------------
+#
+# Written by  oclyke, Feb 2021
+# 
+#
+# More information on qwiic is at https://www.sparkfun.com/qwiic
+#
+# Do you like this library? Help support SparkFun. Buy a board!
+#
+#==================================================================================
+# Copyright (c) 2021 SparkFun Electronics
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# of this software and associated documentation files (the "Software"), to deal 
+# in the Software without restriction, including without limitation the rights 
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+# copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all 
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+# SOFTWARE.
+#==================================================================================
+
+from .i2c_driver import I2CDriver
+
+import sys
+import os
+
+
+_PLATFORM_NAME = "RP2040 MicroPython"
+
+# used internally in this file to get i2c class object 
+def _connectToI2CBus(id, freq=400000):
+	try:
+		from machine import I2C
+		return I2C(id=id, freq=freq)
+	except Exception as e:
+		print(str(e))
+		print('error: failed to connect to i2c bus')
+	return None
+
+class MicroPythonRP2040I2C(I2CDriver):
+
+	# Constructor
+	name = _PLATFORM_NAME
+	_i2cbus = None
+	_i2c_port_id = 0
+
+	def __init__(self):
+		I2CDriver.__init__(self) # init super
+
+	@classmethod
+	def isPlatform(cls):
+		return os.uname().sysname in ['rp2']
+
+
+#-------------------------------------------------------------------------		
+	# General get attribute method
+	#
+	# Used to intercept getting the I2C bus object - so we can perform a lazy
+	# connect ....
+	#
+	def __getattr__(self, name):
+
+		if(name == "i2cbus"):
+			if( self._i2cbus == None):
+				self._i2cbus = _connectToI2CBus(self._i2c_port_id)
+			return self._i2cbus
+
+		else:
+			# Note - we call __getattribute__ to the super class (object).
+			return super(I2CDriver, self).__getattribute__(name)
+
+	#-------------------------------------------------------------------------
+	# General set attribute method
+	#
+	# Basically implemented to make the i2cbus attribute readonly to users 
+	# of this class. 
+	#
+	def __setattr__(self, name, value):
+
+		if(name != "i2cbus"):
+			super(I2CDriver, self).__setattr__(name, value)
+
+	# read commands ----------------------------------------------------------
+	def readWord(self, address, commandCode):
+		buffer = bytearray(2)
+		self.i2cbus.writeto(address, commandCode, False)
+		self.i2cbus.readfrom_into(address, buffer)
+		return (buffer[1] << 8 ) | buffer[0]
+
+	def readByte(self, address, commandCode):
+		buffer = bytearray(1)
+		self.i2cbus.writeto(address, commandCode, False)
+		self.i2cbus.readfrom_into(address, buffer)
+		return buffer
+
+	def readBlock(self, address, commandCode, nBytes):
+		buffer = bytearray(nBytes)
+		self.i2cbus.writeto(address, commandCode, False)
+		self.i2cbus.readfrom_into(address, buffer)
+		return buffer
+
+		
+	# write commands----------------------------------------------------------
+	def writeCommand(self, address, commandCode):
+		self.i2cbus.writeto(address, commandCode)
+
+	def writeWord(self, address, commandCode, value):
+		buffer = bytearray(2)
+		buffer[0] = value & 0xFF
+		buffer[1] = (value >> 8) & 0xFF
+		self.i2cbus.writeto(address, commandCode, False)
+		self.i2cbus.writeto(address, buffer)		
+
+	def writeByte(self, address, commandCode, value):
+		self.i2cbus.writeto(address, commandCode, False)
+		self.i2cbus.writeto(address, bytes(value))		
+
+	def writeBlock(self, address, commandCode, value):
+		data = [value] if isinstance(value, list) else value
+		self.i2cbus.writeto(address, commandCode, False)
+		self.i2cbus.writeto(address, data)
+
+
+	# scan -------------------------------------------------------------------
+	@classmethod
+	def scan(cls):
+		""" Returns a list of addresses for the devices connected to the I2C bus."""
+	
+		if cls._i2cbus == None:
+			cls._i2cbus = _connectToI2CBus(cls._i2c_port_id)
+	
+		if cls._i2cbus == None:
+			return []
+			
+		return cls._i2cbus.scan()
