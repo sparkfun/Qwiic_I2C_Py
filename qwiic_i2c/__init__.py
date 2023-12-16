4a0285b2aaec892f48fd64a4b949f9701200c90e
Kirk Benell <kirk.benell@sparkfun.com>
2023-12-16 08:49:03 -0700

    ran through formatter; added a base class : QwiicDevice, manages i2c bus setup

--- qwiic_i2c/__init__.py
+++ qwiic_i2c/__init__.py
@@ -1,40 +1,40 @@
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # __init__.py
 #
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 #
 #
 # Written by  SparkFun Electronics, May 2019
-# 
-# This python library supports the SparkFun Electroncis qwiic 
+#
+# This python library supports the SparkFun Electronics qwiic
 # qwiic sensor/board ecosystem on a Raspberry Pi (and compatable) single
-# board computers. 
+# board computers.
 #
 # More information on qwiic is at https://www.sparkfun.com/qwiic
 #
 # Do you like this library? Help support SparkFun. Buy a board!
 #
-#==================================================================================
+# ==================================================================================
 # Copyright (c) 2019 SparkFun Electronics
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy 
-# of this software and associated documentation files (the "Software"), to deal 
-# in the Software without restriction, including without limitation the rights 
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-# copies of the Software, and to permit persons to whom the Software is 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all 
+# The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-#==================================================================================
+# ==================================================================================
 
 """
 qwiic_i2c
@@ -61,15 +61,15 @@ New to qwiic? Take a look at the entire [SparkFun qwiic ecosystem](https://www.s
 """
 # Package to abstract the interface to the execution platforms I2C bus for QWIIC.
 #
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Drivers and driver base class
 from .i2c_driver import I2CDriver
 
 # All supported platform module and class names
 _supported_platforms = {
-	"linux_i2c": "LinuxI2C",
-	"circuitpy_i2c": "CircuitPythonI2C",
-	"micropython_i2c": "MicroPythonI2C"
+    "linux_i2c": "LinuxI2C",
+    "circuitpy_i2c": "CircuitPythonI2C",
+    "micropython_i2c": "MicroPythonI2C"
 }
 
 # List of platform drivers found on this system
@@ -78,85 +78,109 @@ _drivers = []
 # Loop through all supported platform drivers, see if they're included, and
 # append them to the driver list if so
 for module_name, class_name in _supported_platforms.items():
-	try:
-		sub_module = __import__("qwiic_i2c." + module_name, None, None, [None])
-		_drivers.append(getattr(sub_module, class_name))
-	except:
-		pass
+    try:
+        sub_module = __import__("qwiic_i2c." + module_name, None, None, [None])
+        _drivers.append(getattr(sub_module, class_name))
+    except:
+        pass
 
 _theDriver = None
 
-#-------------------------------------------------
-# Exported method to get the I2C driver for the execution platform. 
+# -------------------------------------------------
+# Exported method to get the I2C driver for the execution platform.
 #
 # If no driver is found, a None value is returned
 
+
 def getI2CDriver(*args, **argk):
-	"""
-	.. function:: getI2CDriver()
+    """
+    .. function:: getI2CDriver()
 
-		Returns the qwiic I2C driver object for current platform.
+            Returns the qwiic I2C driver object for current platform.
 
-		:return: A qwiic I2C driver object for the current platform.
-		:rtype: object
+            :return: A qwiic I2C driver object for the current platform.
+            :rtype: object
 
-		:example:
+            :example:
 
-		>>> import qwiic_i2c
-		>>> i2cDriver = qwiic_i2c.getI2CDriver()
-		>>> myData = i2cDriver.readByte(0x73, 0x34)
-	"""
+            >>> import qwiic_i2c
+            >>> i2cDriver = qwiic_i2c.getI2CDriver()
+            >>> myData = i2cDriver.readByte(0x73, 0x34)
+    """
 
-	global _theDriver
+    global _theDriver
 
-	if _theDriver != None:
-		return _theDriver
+    if _theDriver != None:
+        return _theDriver
 
-	
+    for driverClass in _drivers:
 
-	for driverClass in _drivers:
+        # Does this class/driver support this platform?
+        if driverClass.isPlatform():
 
-		# Does this class/driver support this platform?
-		if driverClass.isPlatform():
+            _theDriver = driverClass(*args, **argk)
+            # Yes - return the driver object
+            return _theDriver
 
-			_theDriver = driverClass(*args, **argk)
-			# Yes - return the driver object
-			return _theDriver
+    return None
 
-	return None
-
-#-------------------------------------------------
+# -------------------------------------------------
 # Method to determine if a particular device (at the provided address)
 # is connected to the bus.
+
+
 def isDeviceConnected(devAddress, *args, **argk):
-	"""
-	.. function:: isDeviceConnected()
+    """
+    .. function:: isDeviceConnected()
 
-		Function to determine if a particular device (at the provided address)
-		is connected to the bus.
+            Function to determine if a particular device (at the provided address)
+            is connected to the bus.
 
-		:param devAddress: The I2C address of the device to check
+            :param devAddress: The I2C address of the device to check
 
-		:return: True if the device is connected, otherwise False.
-		:rtype: bool
+            :return: True if the device is connected, otherwise False.
+            :rtype: bool
 
-	"""
-	i2c = getI2CDriver(*args, **argk)
+    """
+    i2c = getI2CDriver(*args, **argk)
 
-	if not i2c:
-		print("Unable to load the I2C driver for this device")
-		return False
-	
-	isConnected = False
-	try:
-		# Try to write a byte to the device, command 0x0
-		# If it throws an I/O error - the device isn't connected
-		with i2c as i2cDriver:
-			i2cDriver.writeCommand(devAddress, 0x0)
+    if not i2c:
+        print("Unable to load the I2C driver for this device")
+        return False
 
-			isConnected = True
-	except Exception as ee:
-		print("Error connecting to Device: %X, %s" % (devAddress, ee))
-		pass
+    isConnected = False
+    try:
+        # Try to write a byte to the device, command 0x0
+        # If it throws an I/O error - the device isn't connected
+        with i2c as i2cDriver:
+            i2cDriver.writeCommand(devAddress, 0x0)
 
-	return isConnected
+            isConnected = True
+    except Exception as ee:
+        print("Error connecting to Device: %X, %s" % (devAddress, ee))
+        pass
+
+    return isConnected
+
+##########################################################################
+# Device base class - used to automate I2C bus creation
+#
+
+
+class QwiicDevice(object):
+    """
+    QwiicDevice
+
+    Provides a base class that manages setup of the I2C device for a driver
+    object/class.
+    """
+
+    def __init__(self, i2c_driver=None, *args, **argk):
+
+        if i2c_driver is None:
+            self._i2c = getI2CDriver(*args, **argk)
+            if self._i2c is None:
+                print("Unable to load I2C driver for this platform.")
+                return
+        else:
+            self._i2c = i2c_driver
